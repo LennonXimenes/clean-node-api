cef0bddd03dbab0bf19607110d0743522811d110
Lennon_Ximenes <lennon_ximenes@hotmail.com>
2025-04-29 23:22:31 -0300

    refactor: move server error to a helper method

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,8 +1,16 @@
+import { ServerError } from "../errors/server-error";
 import { iHttpResponse } from "../protocols/http";
 
 export const badRequest = (error: Error): iHttpResponse => {
   return {
     statusCode: 400,
     body: error,
+  };
+};
+
+export const serverError = (): iHttpResponse => {
+  return {
+    statusCode: 500,
+    body: new ServerError(),
   };
 };
